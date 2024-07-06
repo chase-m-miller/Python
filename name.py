3e60d6cc844ed6977ccdd7f677c6e2dd573ce12a
Chase Miller <chasemmiller2@gmail.com>
2024-07-06 13:43:29 -0500

    Add name + age program

---
+++ name.py
@@ -0,0 +1,10 @@
+print('What is your name? ')
+myName = input()
+
+print('Hello, ' + myName + '.')
+print('Your name is ' + str(len(myName)) + ' characters long.')
+
+print('How old are you? ')
+myAge = input()
+
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
