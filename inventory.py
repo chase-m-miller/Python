4f28090d930b18c396b89d7a136495a1e9328a9a
Chase Miller <chasemmiller2@gmail.com>
2024-07-16 18:58:14 -0500

    Add addToInventory function

--- inventory.py
+++ inventory.py
@@ -1,6 +1,3 @@
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-
-
 def printInventory(inventory):
     totalItems = 0
     for k, v in inventory.items():
@@ -9,4 +6,21 @@ def printInventory(inventory):
     print('Total items: ' + str(totalItems))
 
 
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        if item in inventory:
+            inventory[item] += 1
+        else:
+            inventory.setdefault(item, 1)
+    return inventory
+
+
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = {'gold coin': 42, 'rope': 1}
+
+
 printInventory(stuff)
+print()
+addToInventory(inv, dragonLoot)
+printInventory(inv)
