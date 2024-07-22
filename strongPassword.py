39aa2727cacb33935e5c5de2aa348b888a8afd08
Chase Miller <chasemmiller2@gmail.com>
2024-07-21 23:07:03 -0500

    Initial commit

---
+++ strongPassword.py
@@ -0,0 +1,19 @@
+#! python3
+# strongPassword.py - detects if password is strong enough
+import re
+
+capRegex = re.compile(r'[A-Z]+')
+lowRegex = re.compile(r'[a-z]+')
+numRegex = re.compile(r'\d+')
+lenRegex = re.compile(r'(\w|\d){8,}')
+
+print('Enter a password to test: ', end='')
+password = input()
+if (capRegex.match(password) and lowRegex.match(password) and 
+   numRegex.match(password) and lenRegex.match(password)):
+    print('Your password is strong enough.')
+else:
+    print('Your password is weak.')
+    print('''Make sure your password contains upper
+           and lowercase letters, at least one number,
+           and is at least 8 characters long.''')
