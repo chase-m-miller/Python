cac8a9797333249231ca071c9e08be88832ac524
Chase Miller <chasemmiller2@gmail.com>
2024-07-23 16:27:50 -0500

    Consolidate char regex's into one

--- strongPassword.py
+++ strongPassword.py
@@ -2,17 +2,14 @@
 # strongPassword.py - detects if password is strong enough
 import re
 
-capRegex = re.compile(r'[A-Z]+')
-lowRegex = re.compile(r'[a-z]+')
-numRegex = re.compile(r'\d+')
+charRegex = re.compile(r'[A-Z]+[a-z]+\d+')
 lenRegex = re.compile(r'(\w|\d){8,}')
 
 print('Enter a password to test: ', end='')
 password = input()
-if (capRegex.search(password) and lowRegex.search(password) and 
-   numRegex.search(password) and lenRegex.match(password)):
+if (charRegex.search(password) and lenRegex.match(password)):
     print('Your password is strong enough.')
 else:
     print('Your password is weak.')
-    print('''Make sure your password contains upper and lowercase letters,
-at least one number, and is at least 8 characters long.''')
+    print('Make sure your password contains upper and lowercase letters,')
+    print('at least one number, and is at least 8 characters long.')
