5f34bf91cd5a4c9fe675814336fdea322d339ae9
Chase Miller <chasemmiller2@gmail.com>
2024-07-23 15:40:12 -0500

    Fix incorrect pattern matching by switching from .match() to .search() method

--- strongPassword.py
+++ strongPassword.py
@@ -9,14 +9,10 @@ lenRegex = re.compile(r'(\w|\d){8,}')
 
 print('Enter a password to test: ', end='')
 password = input()
-if (capRegex.match(password) and lowRegex.match(password) and 
-   numRegex.match(password) and lenRegex.match(password)):
+if (capRegex.search(password) and lowRegex.search(password) and 
+   numRegex.search(password) and lenRegex.match(password)):
     print('Your password is strong enough.')
 else:
-    print('Capitals? ' + str(bool(capRegex.match(password))))
-    print('Lowercase? ' + str(bool(lowRegex.match(password))))
-    print('Number? ' + str(bool(numRegex.match(password))))
     print('Your password is weak.')
-    print('''Make sure your password contains upper
-and lowercase letters, at least one number,
-and is at least 8 characters long.''')
+    print('''Make sure your password contains upper and lowercase letters,
+at least one number, and is at least 8 characters long.''')
