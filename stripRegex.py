9fc619d9a5c86b77c60946755b7f105e723321ae
Chase Miller <chasemmiller2@gmail.com>
2024-07-23 20:45:39 -0500

    Add option to remove only starting or ending whitespace

--- stripRegex.py
+++ stripRegex.py
@@ -4,13 +4,25 @@
 import re
 
 
-def regexStrip(text):
-    # remove whitespace from beginning of string
-    text = re.sub(r'^\s+', '', text)
-    # remove whitespace from end of string
-    text = re.sub(r'\s+$', '', text)
-    print(text)
+def regexStrip(text, opt=None):
+    if opt == 'start':
+        # remove whitespace from beginning of string
+        text = re.sub(r'^\s+', '', text)
+        print(text)
+        return
+    elif opt == 'end':
+        # remove whitespace from end of string
+        text = re.sub(r'\s+$', '', text)
+        print(text)
+        return
+    else:
+        text = re.sub(r'^\s+', '', text)
+        text = re.sub(r'\s+$', '', text)
+        print(text)
+        return
 
 
 regexStrip('   Does this work?   ')
 regexStrip('\t\t\tHow about this?\t\t\t')
+regexStrip('   Just the start?   ', opt='start')
+regexStrip('   Just the end?   ', opt='end')
