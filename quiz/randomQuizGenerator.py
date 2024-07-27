375426f3a491e3d1fca25a4bb8dc48f67670aed0
Chase Miller <chasemmiller2@gmail.com>
2024-07-27 00:23:10 -0500

    Write the quizs and answer sheets to files

--- quiz/randomQuizGenerator.py
+++ quiz/randomQuizGenerator.py
@@ -48,6 +48,15 @@ for quizNum in range(35):
         answerOptions = wrongAnswers + [correctAnswer]
         random.shuffle(answerOptions)
 
-        # TODO: Write the question and answer options to the quiz file.
+        # Write the question and answer options to the quiz file.
+        quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1,
+            states[questionNum]))
+        for i in range(4):
+            quizFile.write('    %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+        quizFile.write('\n')
 
-        # TODO: Write the answer key to a file.
+        # Write the answer key to a file.
+        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[
+            answerOptions.index(correctAnswer)]))
+    quizFile.close()
+    answerKeyFile.close()
