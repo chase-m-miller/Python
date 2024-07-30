3d389d77f8cf07f2fa84e32966b74f8c0e951157
Chase Miller <chasemmiller2@gmail.com>
2024-07-30 14:58:31 -0500

    Write finished madlib to 'madlib_filled.txt'

--- madlib.py
+++ madlib.py
@@ -1,20 +1,25 @@
 #! python3
+# madlib.py - This program looks in the current directory for a file
+# named 'madlib.txt', and prompts the user to replace any occurences
+# of NOUN, VERB, ADJECTIVE, or ADVERB. The program then prints the
+# new string, writes it to 'madlib_filled.txt', and terminates.
 
 import re
 
+# Regex used for substitutions
 noun = re.compile('NOUN')
 verb = re.compile('VERB')
 adjective = re.compile('ADJECTIVE')
 adverb = re.compile('ADVERB')
 
-# Open and read text file
+# Open and read text file, then split it into a list
 madlibFile = open('madlib.txt', 'r')
 madlibStr = madlibFile.read().split()
 
 # Scan for ADJECTIVE, NOUN, VERB, and ADVERB
+# If found, prompt user for replacement
 index = 0
 for word in madlibStr:
-    print(word)
     if adjective.match(word):
         print('Enter an adjective: ', end='')
         userInput = input()
@@ -31,12 +36,15 @@ for word in madlibStr:
         print('Enter an adverb: ', end='')
         userInput = input()
         madlibStr[index] = re.sub(adverb, userInput, word)
+
     index += 1
 
+# Create a new text file, print new string and write to file
 newString = ' '.join(madlibStr)
 print(newString)
-# TODO: Prompt the user to replace any occurences
 
-# TODO: Create a new text file, print new string and write to file
+madlibCompleted = open('madlib_filled.txt', 'w')
+madlibCompleted.write(newString)
 
 madlibFile.close()
+madlibCompleted.close()
