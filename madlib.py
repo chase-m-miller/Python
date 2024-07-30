1595592ac1797f52737936bf4156a982e383dded
Chase Miller <chasemmiller2@gmail.com>
2024-07-30 00:44:27 -0500

    First commit

---
+++ madlib.py
@@ -0,0 +1,9 @@
+import os
+
+# TODO: Open and read text file
+
+# TODO: Scan for ADJECTIVE, NOUN, VERB, and ADVERB
+
+# TODO: Prompt the user to replace any occurences
+
+# TODO: Create a new text file, print new string and write to file
