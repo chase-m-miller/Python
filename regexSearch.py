994b7bf5633f44f3c882bd0c5e0a12638ff2ddb1
Chase Miller <chasemmiller2@gmail.com>
2024-07-31 19:19:29 -0500

    First commit

---
+++ regexSearch.py
@@ -0,0 +1,13 @@
+#! python3
+# regexSearch.py - This program opens all .txt files in a folder
+# and searches for any line that matches a user-supplied
+# regular expression. The result is printed to the screen.
+
+import re
+
+# TODO: Get regular expression from user.
+
+# TODO: Search cwd for .txt files and open all of them.
+
+# TODO: Search the .txt files for lines matching
+#       regex and print them to the screen.
