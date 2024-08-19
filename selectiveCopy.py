27e703d14ca17d64719f013a6c5ce2e63562c1e6
chase-m-miller <chasemmiller2@gmail.com>
2024-08-19 04:19:50 -0500

    First commit

---
+++ selectiveCopy.py
@@ -0,0 +1,6 @@
+#! python3
+# selectiveCopy.py - This program walks through a folder tree and searches for
+# a certain file extension (such as .pdf or .jpg). The program then copies
+# these files from there current location to a new folder.
+
+
