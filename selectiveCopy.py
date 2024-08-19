64b016e71c3d81baca7dd52fbb79c4d45b139393
chase-m-miller <chasemmiller2@gmail.com>
2024-08-19 04:23:06 -0500

    Add walk boilerplate

--- selectiveCopy.py
+++ selectiveCopy.py
@@ -3,4 +3,18 @@
 # a certain file extension (such as .pdf or .jpg). The program then copies
 # these files from there current location to a new folder.
 
+import os
 
+print('Input a directory to walk: ', end='')
+searchDir = input()
+
+for folderName, subfolders, filenames in os.walk(searchDir):
+    print('The current folder is ' + folderName)
+
+    for subfolder in subfolders:
+        print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+
+    for filename in filenames:
+        print('FILE INSIDE ' + folderName + ': ' + filename)
+
+    print('')
