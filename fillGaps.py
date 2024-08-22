ca5a186909bc980eadb76ff4e797ae54835ed713
chase-m-miller <chasemmiller2@gmail.com>
2024-08-22 06:39:14 -0500

    First commit

---
+++ fillGaps.py
@@ -0,0 +1,4 @@
+#! python3
+# fillGaps.py - This program finds all files with a given prefix and locates
+# any gaps in the numbering (Example: spam001.txt spam002.txt spam004.txt)
+# and renames all later files to close this gap.
