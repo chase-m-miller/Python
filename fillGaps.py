afdde0ecb4c4c8b691c9435a34a4c4994d11221e
Chase Miller <chasemmiller2@gmail.com>
2024-08-25 02:09:28 -0500

    Remove unneeded print statements

--- fillGaps.py
+++ fillGaps.py
@@ -13,14 +13,9 @@ num = 1
 for dirname, foldernames, filenames in os.walk(searchDir):
     for filename in filenames:
         if result := filePrefix.search(filename):
-            if result.group(1) == str(num).zfill(3):
-                print('IN ORDER')
-            else:
-                print('OUT OF ORDER: RENAMING')
+            if result.group(1) != str(num).zfill(3):
                 splitTup = os.path.splitext(filename)
                 fileExtension = splitTup[1]
                 os.rename(os.path.join(searchDir, filename),
                           os.path.join(searchDir, 'spam' + str(num).zfill(3) + fileExtension))
             num = num + 1
-            print(filename)
-            # print(str(1).zfill(3))
