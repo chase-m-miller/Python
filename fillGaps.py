fb8d9f3c80f013f359b7c37626dbc14adcff0dba
Chase Miller <chasemmiller2@gmail.com>
2024-08-24 20:34:29 -0500

    Rename non-sequential numbers in filenames

--- fillGaps.py
+++ fillGaps.py
@@ -13,10 +13,11 @@ num = 1
 for dirname, foldernames, filenames in os.walk(searchDir):
     for filename in filenames:
         if result := filePrefix.search(filename):
-            if result.group(1) == (str(num).zfill(3)):
+            if result.group(1) == str(num).zfill(3):
                 print('IN ORDER')
             else:
-                print('OUT OF ORDER')
+                print('OUT OF ORDER: RENAMING')
+                os.rename(os.path.join(searchDir, filename), os.path.join(searchDir, 'spam' + str(num).zfill(3) + '.txt'))
             num = num + 1
             print(filename)
             # print(str(1).zfill(3))
