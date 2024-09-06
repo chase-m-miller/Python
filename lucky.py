5959cb60872332e107c65b4506b8b5c062e3d452
Chase Miller <chasemmiller2@gmail.com>
2024-09-06 14:34:42 -0500

    Import & enable logging

--- lucky.py
+++ lucky.py
@@ -5,13 +5,17 @@ import requests
 import sys
 import webbrowser
 import bs4
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 
 print('Googling...')  # Display text while downloading the Google page.
+
 res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 # Retrieve top search result links.
-soup = bs4.BeautifulSoup(res.text)
+soup = bs4.BeautifulSoup(res.text, features='html.parser')
 
 # Open a browser tab for each result.
 linkElems = soup.select('.r a')
