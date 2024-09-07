e95daee30ae17431bc63f82237f7c779e9c4acc9
Chase Miller <chasemmiller2@gmail.com>
2024-09-06 22:51:51 -0500

    Add headers to confuse google hopefully

--- lucky.py
+++ lucky.py
@@ -8,18 +8,18 @@ import bs4
 import logging
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
 
 print('Googling...')  # Display text while downloading the Google page.
 
-res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]), headers=headers)
 res.raise_for_status()
 
 # Retrieve top search result links.
 soup = bs4.BeautifulSoup(res.text, features='html.parser')
 
 # Open a browser tab for each result.
-linkElems = soup.select('span > a[href]')
-#linkElems = soup.findAll('a', {'jsname': 'UWckNb'})
+linkElems = soup.select('div[id="search"]')
 logging.debug('Length of link Elems: ' + str(len(linkElems)))
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
