9f29f18885a1b7e7ae8621397710149c98869c46
Chase Miller <chasemmiller2@gmail.com>
2024-09-07 01:02:01 -0500

    Format and add comments

--- lucky.py
+++ lucky.py
@@ -7,8 +7,12 @@ import webbrowser
 import bs4
 import logging
 
+# Configure logging.
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+
+# Add headers to get better Google results.
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
+
 
 print('Googling...')  # Display text while downloading the Google page.
 
@@ -19,7 +23,7 @@ res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text, features='html.parser')
 
 # Open a browser tab for each result.
-linkElems = soup.select('div[id="search"]')
+linkElems = soup.select('.yuRUbf > div > span > a[href]')
 logging.debug('Length of link Elems: ' + str(len(linkElems)))
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
