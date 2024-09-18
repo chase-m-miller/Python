a3024e3818546c81212b209ec76f34e9c6e76b87
chase-m-miller <chasemmiller2@gmail.com>
2024-09-17 19:14:10 -0500

    Import and test selenium

--- 2048bot.py
+++ 2048bot.py
@@ -1,4 +1,8 @@
 #! python3
 # 2048bot.py - This program opens a web browser and automatically plays 2048.
 
-import selenium
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+
+browser.get('https://google.com/')
