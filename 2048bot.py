22170b90418dac12d589a3dee13ffbf8127505ee
Chase Miller <chasemmiller2@gmail.com>
2024-09-17 22:34:08 -0500

    Stop repeating inputs when game ends, then start another

--- 2048bot.py
+++ 2048bot.py
@@ -1,6 +1,8 @@
 #! python3
 # 2048bot.py - This program opens a web browser and automatically plays 2048.
 
+import selenium
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -9,8 +11,17 @@ browser = webdriver.Firefox()
 browser.get('https://gabrielecirulli.github.io/2048/')
 
 htmlElem = browser.find_element('tag name', 'html')
+
+
 while True:
-    htmlElem.send_keys(Keys.UP)
-    htmlElem.send_keys(Keys.DOWN)
-    htmlElem.send_keys(Keys.LEFT)
-    htmlElem.send_keys(Keys.RIGHT)
+    try:
+        restartButton = browser.find_element('link text', 'Try again')
+        print('Sleeping for 5 seconds.')
+        time.sleep(5)
+        restartButton.click()
+    except selenium.common.exceptions.NoSuchElementException:
+        htmlElem.send_keys(Keys.UP)
+        htmlElem.send_keys(Keys.DOWN)
+        htmlElem.send_keys(Keys.LEFT)
+        htmlElem.send_keys(Keys.RIGHT)
+        continue
