fc323e8c187d542a9b8d5f24a164d24a000b6bf6
chase-m-miller <chasemmiller2@gmail.com>
2024-09-17 19:22:42 -0500

    Test send_keys is functioning properly

--- 2048bot.py
+++ 2048bot.py
@@ -2,7 +2,11 @@
 # 2048bot.py - This program opens a web browser and automatically plays 2048.
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Firefox()
 
 browser.get('https://gabrielecirulli.github.io/2048/')
+
+htmlElem = browser.find_element('tag name', 'html')
+htmlElem.send_keys(Keys.END)
