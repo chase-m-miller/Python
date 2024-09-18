8ec3e4cea27c9a324a0796993712e8b3019454cd
Chase Miller <chasemmiller2@gmail.com>
2024-09-18 03:35:41 -0500

    Add comments

--- 2048bot.py
+++ 2048bot.py
@@ -5,16 +5,18 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# Open 2048 in Firefox and select the <html> tag
 browser = webdriver.Firefox()
-
 browser.get('https://gabrielecirulli.github.io/2048/')
-
 htmlElem = browser.find_element('tag name', 'html')
 
 
 while True:
+    # Try to find the 'Try again' button, and prompt the user
+    # to start another game if it is found.
     try:
         restartButton = browser.find_element('link text', 'Try again')
+        # If the game is over, prompt to start another.
         print('Game over. Start another? (y/n): ')
         response = input()
         if response.lower() == 'y':
@@ -22,6 +24,8 @@ while True:
         else:
             browser.close()
             break
+
+    # Executes as long as the 'Try again' button is not found.
     except selenium.common.exceptions.NoSuchElementException:
         htmlElem.send_keys(Keys.UP)
         htmlElem.send_keys(Keys.DOWN)
